11bd49103144df4811bca50dacfb6ed7d891f1dd
hoangtularva <hoangtukg295@gmail.com>
2020-07-21 09:19:35 +0700

    send mail success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(name: params[:user][:name], email: params[:user][:email])
     if @user.save
       flash[:success] = "Register success"
+      UserMailer.welcome_email(@user).deliver_now 
       redirect_to new_user_path
     else
       flash[:danger] = "Register fail"
