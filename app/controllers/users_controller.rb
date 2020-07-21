4c7e4f77caae476bd5c1919b526baca49d8b0cc2
hoangtularva <hoangtukg295@gmail.com>
2020-07-21 09:09:25 +0700

    edit user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      flash[:success] = "Register success"
+      redirect_to new_user_path
+    else
+      flash[:danger] = "Register fail"
+      render :new
+    end
   end
 end
